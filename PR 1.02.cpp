fa381f02109b6f61f00d120059bddd681e5bb546
akaneirya <akaneriya913@gmail.com>
2026-04-06 03:30:17 -0700

    Add files via upload

---
+++ PR 1.02.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+	int base_salary, HRA , DA , TA , gross_salary;
+	
+	printf("enter the value of base salary");
+	scanf("%d",&base_salary);
+	
+	printf("enter the value of HRA");
+	scanf("%d",&HRA);
+	
+	printf("enter the value of DA");
+	scanf("%d",&DA);
+	
+	printf("enter the value of TA");
+	scanf("%d",&TA);
+	
+	printf("enter the value of gross salary");
+	scanf("%d",&gross_salary);
+	
+}
+
+  /*
+  
+  output
+  
+   1)enter the value of base salary100
+   2)enter the value of HRA10
+   3)enter the value of DA5
+   4)enter the value of TA8
+   5)enter the value of gross salary123
